18c94731c0d3d7433674d97d8f3398eabbb9d814
Chris Svenningsen <chris@carbonfive.com>
2013-12-05 13:45:09 -0800

    wip

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'nokogiri'
 gem 'typhoeus'
+gem 'mandrill'
--- home-bus-tracker.rb
+++ home-bus-tracker.rb
@@ -3,6 +3,9 @@
 require 'rubygems'
 require 'typhoeus'
 require 'nokogiri'
+require 'mandrill'
+
+mandrill = Mandrill::API.new ENV['MANDRILL_APIKEY']
 
 response = Typhoeus.get('http://www.nextbus.com/predictor/fancyNewPredictionLayer.jsp?a=lametro&r=720&s=16704')
 html = Nokogiri::HTML(response.body)
@@ -12,4 +15,21 @@ buses << html.css('.predictionNumberForOtherPreds .right').first.content
 buses << html.css('.predictionNumberForOtherPreds .right').last.content
 buses.map! {|b| b.gsub(/\n| /,'') }
 prediction = buses.join(', ')
-puts prediction
+
+message = {
+  message: {
+    subject: "#{prediction}",
+    from_email: 'noreply@example.com',
+    from_name: 'Your friendly neighborhood bus tracker',
+    to: [
+      {
+        email: 'crsven@gmail.com',
+        name: 'Chris Svenningsen',
+        type: 'to',
+      }
+    ],
+  }
+}
+
+result = mandrill.messages.send message
+puts result
