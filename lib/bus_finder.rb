21cbb49b6e0d96108d052cdcb64c587941e9ef69
Chris Svenningsen <chris@carbonfive.com>
2013-12-18 10:30:57 -0800

    Refactor bus finding to only show SM buses.

--- lib/bus_finder.rb
+++ lib/bus_finder.rb
@@ -17,10 +17,10 @@ class BusFinder
     return unless within_finding_time
 
     response = Typhoeus.get(BUS_URL)
-    html = Nokogiri::HTML(response.body)
-    @buses << html.css('.predictionNumberForFirstPred .right').first.content
-    @buses << html.css('.predictionNumberForOtherPreds .right').first.content
-    @buses << html.css('.predictionNumberForOtherPreds .right').last.content
+    @html = Nokogiri::HTML(response.body)
+    @buses << first_bus_time if first_bus_to_sm?
+    @buses << second_bus_time if second_bus_to_sm?
+    @buses << third_bus_time if third_bus_to_sm?
     @buses.map! {|b| b.gsub(/\n| /,'') }
   end
 
@@ -31,5 +31,44 @@ class BusFinder
     return false unless (time.hour == @finder_start_hour && time.min >= @finder_start_min) ||
       (time.hour == @finder_stop_hour && time.min <= @finder_stop_min)
     true
+  end
+
+  def first_bus_selector
+    @html.css('.predictionNumberForFirstPred .right').first
+  end
+
+  def second_bus_selector
+    @html.css('.predictionNumberForOtherPreds .right').first
+  end
+
+  def third_bus_selector
+    @html.css('.predictionNumberForOtherPreds .right').last
+  end
+
+  def first_bus_time
+    return 'Arriving' unless first_bus_selector
+    first_bus_selector.content
+  end
+
+  def first_bus_to_sm?
+    @html.css('.directionNameForPred').first.content.include? 'Santa Monica'
+  end
+
+  def second_bus_time
+    return 'Arriving' unless second_bus_selector
+    second_bus_selector.content
+  end
+
+  def second_bus_to_sm?
+    @html.css('.directionNameForPred')[1].content.include? 'Santa Monica'
+  end
+
+  def third_bus_time
+    return 'Arriving' unless third_bus_selector
+    third_bus_selector.content
+  end
+
+  def third_bus_to_sm?
+    @html.css('.directionNameForPred').last.content.include? 'Santa Monica'
   end
 end
